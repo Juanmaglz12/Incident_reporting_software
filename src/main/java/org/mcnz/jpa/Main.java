8ad03ad40f9d76d97ab54aae4d4dbf37157ead5e
Juanmaglz12 <juanmaglz617@gmail.com>
2023-12-04 00:58:13 -0300

    Pequeños arreglos

--- src/main/java/org/mcnz/jpa/Main.java
+++ src/main/java/org/mcnz/jpa/Main.java
@@ -1,33 +1,16 @@
 package org.mcnz.jpa;
 
 import org.mcnz.jpa.methods.MenuMethods;
-import org.mcnz.jpa.methods.ModuloMesaDeAyuda;
-import org.mcnz.jpa.methods.ModuloRRHH;
-import org.mcnz.jpa.methods.initialization.Initialization;
-import org.mcnz.jpa.models.users.HelpDeskUser;
+
 
 public class Main {
     public static void main(String[] args) {
-
-
-
-        /*Metodo para poblar la base de datos.
-          Ejecutar una vez y volver a comentar.*/
-
-        //Initialization.initialization();
 
         System.out.println("********************************");
         System.out.println("Bienvenido al sistema de tickets");
         System.out.println("********************************");
 
         MenuMethods.menu();
-
-
-
-
-
-
-
 
     }
 }
